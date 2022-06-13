2a11a85ad6809f86fc50af363f688d0d5f6abea6
VardanTuladhar <tuladharvr@vcu.edu>
2022-06-13 01:09:40 -0400

    completed
    using scanf and a case statement we made a simple calculator with only two float and a operator

--- simple_calculator.c
+++ simple_calculator.c
@@ -2,8 +2,33 @@
 #include <stdlib.h>
 int main(void)
 {
-    float input1, input2;
+    float input1, input2, answer;
     char operator[1];
+
+    printf("type expression: \n");
+
+    scanf("%f %s %f", &input1, &operator,  &input2); // space needed between inputs
+
+    //printf("%s", operator);
+    switch (operator[0])
+    {
+        case '+':
+            answer = input1 + input2;
+            break;
+        case '-':
+            answer = input1 - input2;
+            break;
+        case '*':
+            answer = input1 * input2;
+            break;
+        case '/':
+            answer = input1 / input2;
+            break;
+        default:
+            printf("Error");
+    }
+    printf("%f", answer);
+
 
 
 }
